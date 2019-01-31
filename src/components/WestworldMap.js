9b7b22550c02ec5a242ae1628e94e65ef4f61a9a
Ann Duong <39737217+thuyanduong-flatiron@users.noreply.github.com>
2019-01-31 14:58:03 -0500

    Update WestworldMap.js

--- src/components/WestworldMap.js
+++ src/components/WestworldMap.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Segment } from 'semantic-ui-react';
 
+//This component was given to you as a functional component, but you can always refactor. You can make it class or functional, what makes the most sense to you?
 const WestworldMap = () => {
 
   return (
