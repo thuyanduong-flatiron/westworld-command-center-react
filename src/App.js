e5110097171a8f6a57fa3ce19f09ab41de61419c
thuyanduong-flatiron <thuyan.duong@flatironschool.com>
2019-01-31 13:11:05 -0500

    started code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
 import './stylesheets/App.css'
 import { Segment } from 'semantic-ui-react';
-import WestWorldMap from './components/WestworldMap'
-import HeadQuarters from './components/Headquarters'
+import WestworldMap from './components/WestworldMap';
+import Headquarters from './components/Headquarters';
 
 class App extends Component {
 
   // As you go through the components given you'll see a lot of functional components.
   // But feel free to change them to whatever you want.
   // It's up to you whether they should be stateful or not.
-  state = {
-    hosts: []
-  }
-
-  componentDidMount(){
-    fetch(`http://localhost:4000/hosts`)
-    .then(res =>res.json())
-    .then(hosts => {
-      this.setState({hosts: hosts})
-    })
-  }
 
   render(){
     return (
       <Segment id='app'>
-          <WestWorldMap hosts={this.state.hosts}/>
-          <HeadQuarters hosts={this.state.hosts}/>
+        <WestworldMap />
+        <Headquarters />
       </Segment>
     )
   }
